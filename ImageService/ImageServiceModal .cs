5668252440b053bd4fea10f2d50cc67ada825bd0
אלעד <@ELAD>
2018-03-24 06:46:48 +0300

    f

---
+++ ImageService/ImageServiceModal .cs
@@ -0,0 +1,46 @@
+﻿using ImageService.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageService
+{
+    public class ImageServiceModal : IImageServiceModal
+    {
+        #region Members
+        private string m_OutputFolder;            // The Output Folder
+        private int m_thumbnailSize;              // The Size Of The Thumbnail Size
+
+        public string AddFile(string path, out bool result)
+        {
+            DateTime dateTime = GetDateTakenFromImage(path);
+            throw new NotImplementedException();
+        }
+
+        //we init this once so that if the function is repeatedly called
+        //it isn't stressing the garbage man
+        private static Regex r = new Regex(":");
+
+        //retrieves the datetime WITHOUT loading the whole image
+        public static DateTime GetDateTakenFromImage(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (Image myImage = Image.FromStream(fs, false, false))
+            {
+                PropertyItem propItem = myImage.GetPropertyItem(36867);
+                string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                return DateTime.Parse(dateTaken);
+            }
+        }
+
+        #endregion
+
+    }
+}
