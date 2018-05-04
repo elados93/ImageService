a155df59543d2a9ea6820fd1a723619b7a8d90bb
shahar palmor <shaharpalmor@gmail.com>
2018-05-04 09:19:32 +0300

    entry

--- ImageServiceGUI/Model/SettingsModel.cs
+++ ImageServiceGUI/Model/SettingsModel.cs
@@ -1,25 +1,21 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ImageServiceGUI.Model
 {
     class SettingsModel : ISettingsModel
     {
-        #region Members
+        private string m_handler;
         private string m_outputDir;
         private string m_sourceName;
         private string m_logName;
         private int m_thumbNailsSize;
-        #endregion
 
-        public ObservableCollection<string> Handlers { get; set; }
         public event PropertyChangedEventHandler PropertyChanged;
-
-        public SettingsModel()
-        {
-            Handlers = new ObservableCollection<string>();
-        }
-
         protected void OnPropertyChanged(string name)
         {
             if (PropertyChanged != null)
@@ -27,18 +23,14 @@ namespace ImageServiceGUI.Model
         }
 
 
-        public int ThumbNailsSize
-        {
-            get { return m_thumbNailsSize; }
+        public int ThumbNailsSize { get { return m_thumbNailsSize; }
             set
             {
                 m_thumbNailsSize = value;
                 OnPropertyChanged("ThumbNailsSize");
             }
         }
-        public string LogName
-        {
-            get { return m_logName; }
+        public string LogName { get {return m_logName; }
             set
             {
                 m_logName = value;
@@ -47,25 +39,32 @@ namespace ImageServiceGUI.Model
         }
         public string SourceName
         {
-            get { return m_sourceName; }
-            set
+            get { return m_sourceName; } set
             {
                 m_sourceName = value;
                 OnPropertyChanged("SourceName");
 
             }
         }
-
         public string OutputDir
         {
-            get { return m_outputDir; }
-            set
+            get {return m_outputDir;} set
             {
                 m_outputDir = value;
                 OnPropertyChanged("OutputDir");
 
             }
         }
+        public string Handler
+        {
+            get { return m_handler; } set
+            {
+                m_handler = value;
+                OnPropertyChanged("Handler");
 
+            }
+        }
+
+      
     }
 }
