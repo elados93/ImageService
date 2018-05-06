a99e9a2bf4df139d1341a8c859f79b468b6c2971
shahar palmor <shaharpalmor@gmail.com>
2018-05-06 12:44:05 +0300

    added log command

---
+++ ImageService/ImageService/Commands/LogCommand.cs
@@ -0,0 +1,36 @@
+﻿using ImageService.Commands;
+using ImageService.Infrastructure.AppConfig;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.ImageService.Commands
+{
+    class LogCommand : ICommand
+    {
+        public string Execute(string[] args, out bool result)
+        {
+            AppConfigParser appConfig = new AppConfigParser();
+            EventLog log = new EventLog(appConfig.logName, ".");
+            EventLogEntryCollection entries = log.Entries;
+            List<string> logEntries = new List<string>();
+            for (int i = 0; i < entries.Count; i++)
+            {
+                logEntries.Add(entries[i].Message.ToString());
+            }
+            string convertEachString;
+            if((convertEachString = JsonConvert.SerializeObject(logEntries)) == null)
+            {
+                result = false;
+                return "convertion to string failed";
+            }
+
+            result = true;
+            return convertEachString;
+        }
+    }
+}
