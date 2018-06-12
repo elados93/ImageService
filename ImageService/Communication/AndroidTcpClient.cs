8f5a27112db573036640762d3e4a924a53527cec
shahar palmor <shaharpalmor@gmail.com>
2018-06-12 13:39:31 +0300

    android tcp client

---
+++ ImageService/Communication/AndroidTcpClient.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageService.Communication
+{
+    class AndroidTcpClient : IImageServiceClient
+    {
+        private AndroidTcpClient() { }
+
+        private static AndroidTcpClient instance;
+
+        public AndroidTcpClient Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AndroidTcpClient();
+                }
+                return instance;
+            }
+        }
+
+        public bool ClientConnected { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void recieveCommand()
+        {
+            new Task(() =>
+            {
+
+                NetworkStream stream = client.GetStream();
+                BinaryReader reader = new BinaryReader(stream);
+
+                while (!stopped)
+                {
+                    try
+                    {
+                        string response = reader.ReadString(); // Wait for response from server
+                        MessageCommand msg = JsonConvert.DeserializeObject<MessageCommand>(response);
+                        Debug.WriteLine($"Got message: {msg.CommandID} from Server");
+                        UpdateAllModels?.Invoke(msg);
+
+                        Thread.Sleep(100); // Update information every 0.1 seconds
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.WriteLine(e.Message);
+                    }
+                }
+            }).Start();
+        }
+    }
+}
---
+++ ImageService/Communication/IImageServiceClient.cs
@@ -0,0 +1,13 @@
+﻿using Communication;
+
+namespace ImageService.Communication
+{
+    /// <summary>
+    /// Interface for every service client.
+    /// </summary>
+    public interface IImageServiceClient
+    {
+        void recieveCommand();
+        bool ClientConnected { get; set; }
+    }
+}
